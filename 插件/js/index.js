ea90e4012255e5048de8aa589564f019b06cd766
anissa18352869671 <39507816+anissa18352869671@users.noreply.github.com>
2018-05-27 09:31:59 +0800

    Add files via upload

---
+++ 插件/js/index.js
@@ -0,0 +1,29 @@
+$.extend({
+	myajax:function(option){
+		$.ajax({
+			type:option.type||"get",
+			url:option.url,
+			data:option.data||null,
+			cache:option.cache||false,
+			success:function(data){
+				if(data){
+					if(option.success) option.success(data);
+				}
+			},
+			beforeSend:function(xhr){
+				if(option.beforeSend) option.beforeSend(xhr);
+				$("#loading").show()
+			},
+			error:function(e){
+				if(option.error) option.error(xhr);
+				console.log(e)
+			},
+			complete:function(){
+				if(option.complete) option.error(complete);
+				$("#loading").hide()
+				
+				
+			}
+		});
+	}
+})
